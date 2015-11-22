ade75dbcb6c53753d993bb73f06c6d0351464214
Dean Biele <dean@invisionlearning.com>
2015-11-22 10:05:24 -0500

    cleaned up

--- scripts/createjsevent.ts
+++ scripts/createjsevent.ts
@@ -1,5 +1,5 @@
 
-export class iEvent {
+export class Event {
 
     private type: string;
     private target:any = null;
@@ -7,46 +7,127 @@ export class iEvent {
     private eventPhase:number = 0;
     private bubbles: boolean;
     private cancelable: boolean;
-    //private timeStamp: number = new Date().getTime();
     private timeStamp: number = new Date().getTime();
     private defaultPrevented: boolean = false;
     private propagationStopped: boolean = false;
     private immediatePropagationStopped: boolean = false;
     private removed: boolean = false;
 
+    // constructor:
+    /**
+	 * Contains properties and methods shared by all events for use with
+	 * {{#crossLink "EventDispatcher"}}{{/crossLink}}.
+	 * 
+	 * Note that Event objects are often reused, so you should never
+	 * rely on an event object's state outside of the call stack it was received in.
+	 * @class Event
+	 * @param {String} type The event type.
+	 * @param {Boolean} bubbles Indicates whether the event will bubble through the display list.
+	 * @param {Boolean} cancelable Indicates whether the default behaviour of this event can be cancelled.
+	 * @constructor
+	 **/
+
     constructor(type: string, bubbles: boolean, cancelable: boolean) {
-        console.log('ievent');
+        this.type = type;
+        // dont like using !! for a type conversion. Personal choice
+        //this.bubbles = !!bubbles;
+        //this.cancelable = !!cancelable;
+        if (bubbles !== undefined) {
+            this.bubbles = Boolean(bubbles)
+        };
+        if (cancelable !== undefined) {
+            this.cancelable = Boolean(cancelable)
+        };
+        
     }
 
+    /**
+	 * <strong>REMOVED</strong>. Removed in favor of using `MySuperClass_constructor`.
+	 * See {{#crossLink "Utility Methods/extend"}}{{/crossLink}} and {{#crossLink "Utility Methods/promote"}}{{/crossLink}}
+	 * for details.
+	 *
+	 * There is an inheritance tutorial distributed with EaselJS in /tutorials/Inheritance.
+	 *
+	 * @method initialize
+	 * @protected
+	 * @deprecated
+	 */
+    // p.initialize = function() {}; // searchable for devs wondering where it is.
+
+    // public methods:
+    /**
+	 * Sets {{#crossLink "Event/defaultPrevented"}}{{/crossLink}} to true if the event is cancelable.
+	 * Mirrors the DOM level 2 event standard. In general, cancelable events that have `preventDefault()` called will
+	 * cancel the default behaviour associated with the event.
+	 * @method preventDefault
+	 **/
     public preventDefault():void {
         this.defaultPrevented = this.cancelable && true;
     };
 
+    /**
+	 * Sets {{#crossLink "Event/propagationStopped"}}{{/crossLink}} to true.
+	 * Mirrors the DOM event standard.
+	 * @method stopPropagation
+	 **/
     public stopPropagation():void {
         this.propagationStopped = true;
     };
 
-
+    /**
+	 * Sets {{#crossLink "Event/propagationStopped"}}{{/crossLink}} and
+	 * {{#crossLink "Event/immediatePropagationStopped"}}{{/crossLink}} to true.
+	 * Mirrors the DOM event standard.
+	 * @method stopImmediatePropagation
+	 **/
     public stopImmediatePropagation():void {
         this.immediatePropagationStopped = this.propagationStopped = true;
     };
 
+    /**
+	 * Causes the active listener to be removed via removeEventListener();
+	 * 
+	 * 		myBtn.addEventListener("click", function(evt) {
+	 * 			// do stuff...
+	 * 			evt.remove(); // removes this listener.
+	 * 		});
+	 * 
+	 * @method remove
+	 **/
     public remove():void {
         this.removed = true;
     };
 
-    public clone():iEvent {
-        return new iEvent(this.type, this.bubbles, this.cancelable);
+    /**
+	 * Returns a clone of the Event instance.
+	 * @method clone
+	 * @return {Event} a clone of the Event instance.
+	 **/
+    public clone():Event {
+        return new Event(this.type, this.bubbles, this.cancelable);
     };
 
-    public set(props: Object): iEvent {
+    /**
+	 * Provides a chainable shortcut method for setting a number of properties on the instance.
+	 *
+	 * @method set
+	 * @param {Object} props A generic object containing properties to copy to the instance.
+	 * @return {Event} Returns the instance the method is called on (useful for chaining calls.)
+	 * @chainable
+	*/
+    public set(props: Object): Event {
         for (var n in props) {
             this[n] = props[n];
         }
         return this;
     };
 
+    /**
+	 * Returns a string representation of this object.
+	 * @method toString
+	 * @return {String} a string representation of the instance.
+	 **/
     public toString():string {
-        return "[iEvent (type=" + this.type + ")]";
+        return "[Event (type=" + this.type + ")]";
     };
 }
--- scripts/createjseventdispatcher.ts
+++ scripts/createjseventdispatcher.ts
@@ -1,12 +1,95 @@
-import {iEvent} from 'utility/createjsevent';
-export class iEventDispatcher {
+import {Event} from 'please update this location. Example: utility/createjsevent';
+export class EventDispatcher {
 
-    public _listeners = null;
+    public _listeners:any = null;
 
-    public _captureListeners = null;
+    public _captureListeners:any = null;
 
+
+    // constructor:
+    /**
+	 * EventDispatcher provides methods for managing queues of event listeners and dispatching events.
+	 *
+	 * You can either extend EventDispatcher or mix its methods into an existing prototype or instance by using the
+	 * EventDispatcher {{#crossLink "EventDispatcher/initialize"}}{{/crossLink}} method.
+	 * 
+	 * Together with the CreateJS Event class, EventDispatcher provides an extended event model that is based on the
+	 * DOM Level 2 event model, including addEventListener, removeEventListener, and dispatchEvent. It supports
+	 * bubbling / capture, preventDefault, stopPropagation, stopImmediatePropagation, and handleEvent.
+	 * 
+	 * EventDispatcher also exposes a {{#crossLink "EventDispatcher/on"}}{{/crossLink}} method, which makes it easier
+	 * to create scoped listeners, listeners that only run once, and listeners with associated arbitrary data. The 
+	 * {{#crossLink "EventDispatcher/off"}}{{/crossLink}} method is merely an alias to
+	 * {{#crossLink "EventDispatcher/removeEventListener"}}{{/crossLink}}.
+	 * 
+	 * Another addition to the DOM Level 2 model is the {{#crossLink "EventDispatcher/removeAllEventListeners"}}{{/crossLink}}
+	 * method, which can be used to listeners for all events, or listeners for a specific event. The Event object also 
+	 * includes a {{#crossLink "Event/remove"}}{{/crossLink}} method which removes the active listener.
+	 *
+	 * <h4>Example</h4>
+	 * Add EventDispatcher capabilities to the "MyClass" class.
+	 *
+	 *      EventDispatcher.initialize(MyClass.prototype);
+	 *
+	 * Add an event (see {{#crossLink "EventDispatcher/addEventListener"}}{{/crossLink}}).
+	 *
+	 *      instance.addEventListener("eventName", handlerMethod);
+	 *      function handlerMethod(event) {
+	 *          console.log(event.target + " Was Clicked");
+	 *      }
+	 *
+	 * <b>Maintaining proper scope</b><br />
+	 * Scope (ie. "this") can be be a challenge with events. Using the {{#crossLink "EventDispatcher/on"}}{{/crossLink}}
+	 * method to subscribe to events simplifies this.
+	 *
+	 *      instance.addEventListener("click", function(event) {
+	 *          console.log(instance == this); // false, scope is ambiguous.
+	 *      });
+	 *      
+	 *      instance.on("click", function(event) {
+	 *          console.log(instance == this); // true, "on" uses dispatcher scope by default.
+	 *      });
+	 * 
+	 * If you want to use addEventListener instead, you may want to use function.bind() or a similar proxy to manage
+	 * scope.
+	 *
+	 * <b>Browser support</b>
+	 * The event model in CreateJS can be used separately from the suite in any project, however the inheritance model
+	 * requires modern browsers (IE9+).
+	 *      
+	 *
+	 * @class EventDispatcher
+	 * @constructor
+	 **/
     constructor() { }
 
+
+        /**
+	 * <strong>REMOVED</strong>. Removed in favor of using `MySuperClass_constructor`.
+	 * See {{#crossLink "Utility Methods/extend"}}{{/crossLink}} and {{#crossLink "Utility Methods/promote"}}{{/crossLink}}
+	 * for details.
+	 *
+	 * There is an inheritance tutorial distributed with EaselJS in /tutorials/Inheritance.
+	 *
+	 * @method initialize
+	 * @protected
+	 * @deprecated
+	 */
+    // p.initialize = function() {}; // searchable for devs wondering where it is.
+
+
+    // static public methods:
+    /**
+	 * Static initializer to mix EventDispatcher methods into a target object or prototype.
+	 * 
+	 * 		EventDispatcher.initialize(MyClass.prototype); // add to the prototype of the class
+	 * 		EventDispatcher.initialize(myObject); // add to a specific instance
+	 * 
+	 * @method initialize
+	 * @static
+	 * @param {Object} target The target object to inject EventDispatcher methods into. This can be an instance or a
+	 * prototype.
+	 **/
     initialize(target: Object): void {
         target['addEventListener'] = this.addEventListener;
         target['on'] = this.on;
@@ -18,18 +101,37 @@ export class iEventDispatcher {
         target['willTrigger'] = this.willTrigger;
     }
 
+    // public methods:
+    /**
+	 * Adds the specified event listener. Note that adding multiple listeners to the same function will result in
+	 * multiple callbacks getting fired.
+	 *
+	 * <h4>Example</h4>
+	 *
+	 *      displayObject.addEventListener("click", handleClick);
+	 *      function handleClick(event) {
+	 *         // Click happened.
+	 *      }
+	 *
+	 * @method addEventListener
+	 * @param {String} type The string type of the event.
+	 * @param {Function | Object} listener An object with a handleEvent method, or a function that will be called when
+	 * the event is dispatched.
+	 * @param {Boolean} [useCapture] For events that bubble, indicates whether to listen for the event in the capture or bubbling/target phase.
+	 * @return {Function | Object} Returns the listener for chaining or assignment.
+	 **/
     addEventListener(type: string, listener: (eventObj: Object) => boolean, useCapture?: boolean): Function;
     addEventListener(type: string, listener: (eventObj: Object) => void, useCapture?: boolean): Function;
     addEventListener(type: string, listener: { handleEvent: (eventObj: Object) => boolean; }, useCapture?: boolean): Object;
     addEventListener(type: string, listener: { handleEvent: (eventObj: Object) => void; }, useCapture?: boolean): Object;
     addEventListener(type, listener, useCapture) {
-        var listeners;
+        let listeners:any;
         if (useCapture) {
             listeners = this._captureListeners = this._captureListeners || {};
         } else {
             listeners = this._listeners = this._listeners || {};
         }
-        var arr = listeners[type];
+        let arr = listeners[type];
         if (arr) { this.removeEventListener(type, listener, useCapture); }
         arr = listeners[type]; // remove may have deleted the array
         if (!arr) { listeners[type] = [listener]; }
@@ -37,6 +139,40 @@ export class iEventDispatcher {
         return listener;
     };
 
+    /**
+	 * A shortcut method for using addEventListener that makes it easier to specify an execution scope, have a listener
+	 * only run once, associate arbitrary data with the listener, and remove the listener.
+	 * 
+	 * This method works by creating an anonymous wrapper function and subscribing it with addEventListener.
+	 * The wrapper function is returned for use with `removeEventListener` (or `off`).
+	 * 
+	 * <b>IMPORTANT:</b> To remove a listener added with `on`, you must pass in the returned wrapper function as the listener, or use
+	 * {{#crossLink "Event/remove"}}{{/crossLink}}. Likewise, each time you call `on` a NEW wrapper function is subscribed, so multiple calls
+	 * to `on` with the same params will create multiple listeners.
+	 * 
+	 * <h4>Example</h4>
+	 * 
+	 * 		var listener = myBtn.on("click", handleClick, null, false, {count:3});
+	 * 		function handleClick(evt, data) {
+	 * 			data.count -= 1;
+	 * 			console.log(this == myBtn); // true - scope defaults to the dispatcher
+	 * 			if (data.count == 0) {
+	 * 				alert("clicked 3 times!");
+	 * 				myBtn.off("click", listener);
+	 * 				// alternately: evt.remove();
+	 * 			}
+	 * 		}
+	 * 
+	 * @method on
+	 * @param {String} type The string type of the event.
+	 * @param {Function | Object} listener An object with a handleEvent method, or a function that will be called when
+	 * the event is dispatched.
+	 * @param {Object} [scope] The scope to execute the listener in. Defaults to the dispatcher/currentTarget for function listeners, and to the listener itself for object listeners (ie. using handleEvent).
+	 * @param {Boolean} [once=false] If true, the listener will remove itself after the first time it is triggered.
+	 * @param {*} [data] Arbitrary data that will be included as the second parameter when the listener is called.
+	 * @param {Boolean} [useCapture=false] For events that bubble, indicates whether to listen for the event in the capture or bubbling/target phase.
+	 * @return {Function} Returns the anonymous function that was created and assigned as the listener. This is needed to remove the listener later using .removeEventListener.
+	 **/
     on(type: string, listener: (eventObj: Object) => boolean, scope?: Object, once?: boolean, data?: any, useCapture?: boolean): Function;
     on(type: string, listener: (eventObj: Object) => void, scope?: Object, once?: boolean, data?: any, useCapture?: boolean): Function;
     on(type: string, listener: { handleEvent: (eventObj: Object) => boolean; }, scope?: Object, once?: boolean, data?: any, useCapture?: boolean): Object;
@@ -53,17 +189,33 @@ export class iEventDispatcher {
         }, useCapture);
     };
 
+    /**
+	 * Removes the specified event listener.
+	 *
+	 * <b>Important Note:</b> that you must pass the exact function reference used when the event was added. If a proxy
+	 * function, or function closure is used as the callback, the proxy/closure reference must be used - a new proxy or
+	 * closure will not work.
+	 *
+	 * <h4>Example</h4>
+	 *
+	 *      displayObject.removeEventListener("click", handleClick);
+	 *
+	 * @method removeEventListener
+	 * @param {String} type The string type of the event.
+	 * @param {Function | Object} listener The listener function or object.
+	 * @param {Boolean} [useCapture] For events that bubble, indicates whether to listen for the event in the capture or bubbling/target phase.
+	 **/
     removeEventListener(type: string, listener: (eventObj: Object) => boolean, useCapture?: boolean): void;
     removeEventListener(type: string, listener: (eventObj: Object) => void, useCapture?: boolean): void;
     removeEventListener(type: string, listener: { handleEvent: (eventObj: Object) => boolean; }, useCapture?: boolean): void;
     removeEventListener(type: string, listener: { handleEvent: (eventObj: Object) => void; }, useCapture?: boolean): void;
     removeEventListener(type: string, listener: Function, useCapture?: boolean): void; // It is necessary for "arguments.callee"
     removeEventListener(type, listener, useCapture): void {
-        var listeners = useCapture ? this._captureListeners : this._listeners;
+        let listeners = useCapture ? this._captureListeners : this._listeners;
         if (!listeners) { return; }
         var arr = listeners[type];
         if (!arr) { return; }
-        for (var i = 0, l = arr.length; i < l; i++) {
+        for (let i = 0, l = arr.length; i < l; i++) {
             if (arr[i] == listener) {
                 if (l == 1) { delete (listeners[type]); } // allows for faster checks.
                 else { arr.splice(i, 1); }
@@ -72,10 +224,34 @@ export class iEventDispatcher {
         }
     };
 
-
+    /**
+	 * A shortcut to the removeEventListener method, with the same parameters and return value. This is a companion to the
+	 * .on method.
+	 * 
+	 * <b>IMPORTANT:</b> To remove a listener added with `on`, you must pass in the returned wrapper function as the listener. See 
+	 * {{#crossLink "EventDispatcher/on"}}{{/crossLink}} for an example.
+	 *
+	 * @method off
+	 * @param {String} type The string type of the event.
+	 * @param {Function | Object} listener The listener function or object.
+	 * @param {Boolean} [useCapture] For events that bubble, indicates whether to listen for the event in the capture or bubbling/target phase.
+	 **/
     off = removeEventListener;
 
-
+    /**
+	 * Removes all listeners for the specified type, or all listeners of all types.
+	 *
+	 * <h4>Example</h4>
+	 *
+	 *      // Remove all listeners
+	 *      displayObject.removeAllEventListeners();
+	 *
+	 *      // Remove all click listeners
+	 *      displayObject.removeAllEventListeners("click");
+	 *
+	 * @method removeAllEventListeners
+	 * @param {String} [type] The string type of the event. If omitted, all listeners for all types will be removed.
+	 **/
     removeAllEventListeners(type?: string): void {
         if (!type) { this._listeners = this._captureListeners = null; }
         else {
@@ -84,17 +260,40 @@ export class iEventDispatcher {
         }
     };
 
+    /**
+	 * Dispatches the specified event to all listeners.
+	 *
+	 * <h4>Example</h4>
+	 *
+	 *      // Use a string event
+	 *      this.dispatchEvent("complete");
+	 *
+	 *      // Use an Event instance
+	 *      var event = new createjs.Event("progress");
+	 *      this.dispatchEvent(event);
+	 *
+	 * @method dispatchEvent
+	 * @param {Object | String | Event} eventObj An object with a "type" property, or a string type.
+	 * While a generic object will work, it is recommended to use a CreateJS Event instance. If a string is used,
+	 * dispatchEvent will construct an Event instance if necessary with the specified type. This latter approach can
+	 * be used to avoid event object instantiation for non-bubbling events that may not have any listeners.
+	 * @param {Boolean} [bubbles] Specifies the `bubbles` value when a string was passed to eventObj.
+	 * @param {Boolean} [cancelable] Specifies the `cancelable` value when a string was passed to eventObj.
+	 * @return {Boolean} Returns false if `preventDefault()` was called on a cancelable event, true otherwise.
+	 **/
     dispatchEvent(eventObj: Object, target?: Object): boolean;
     dispatchEvent(eventObj: string, target?: Object): boolean;
     dispatchEvent(eventObj: Event, target?: Object): boolean;
     public dispatchEvent(eventObj, target): boolean {
         if (typeof eventObj == "string") {
             // skip everything if there's no listeners and it doesn't bubble:
-            var listeners = this._listeners;
+            let listeners = this._listeners;
             //if (!bubbles && (!listeners || !listeners[eventObj])) { return true; }
-            if ((!listeners || !listeners[eventObj])) { return true; }
+            if ((!listeners || !listeners[eventObj])) {
+                return true;
+            }
             //eventObj = new iEvent(eventObj, bubbles, cancelable);
-            eventObj = new iEvent(eventObj, false, false);
+            eventObj = new Event(eventObj, false, false);
         } else if (eventObj.target && eventObj.clone) {
             // redispatching an active event object, so clone it:
             eventObj = eventObj.clone();
@@ -105,12 +304,12 @@ export class iEventDispatcher {
         if (!eventObj.bubbles || !parent) {
             this._dispatchEvent(eventObj, 2);
         } else {
-            var top = this;
-            var list = [top];
+            let top = this;
+            let list = [top];
             while (top['parent']) {
                 list.push(top = top['parent']);
             }
-            var i, l = list.length;
+            let i, l = list.length;
 
             // capture & atTarget
             for (i = l - 1; i >= 0 && !eventObj.propagationStopped; i--) {
@@ -124,14 +323,30 @@ export class iEventDispatcher {
         return !eventObj.defaultPrevented;
     };
 
-
+    /**
+	 * Indicates whether there is at least one listener for the specified event type.
+	 * @method hasEventListener
+	 * @param {String} type The string type of the event.
+	 * @return {Boolean} Returns true if there is at least one listener for the specified event.
+	 **/
     hasEventListener(type: string): boolean {
-        var listeners = this._listeners, captureListeners = this._captureListeners;
+        let listeners:any = this._listeners, captureListeners = this._captureListeners;
         return !!((listeners && listeners[type]) || (captureListeners && captureListeners[type]));
     };
 
+    /**
+	 * Indicates whether there is at least one listener for the specified event type on this object or any of its
+	 * ancestors (parent, parent's parent, etc). A return value of true indicates that if a bubbling event of the
+	 * specified type is dispatched from this object, it will trigger at least one listener.
+	 * 
+	 * This is similar to {{#crossLink "EventDispatcher/hasEventListener"}}{{/crossLink}}, but it searches the entire
+	 * event flow for a listener, not just this object.
+	 * @method willTrigger
+	 * @param {String} type The string type of the event.
+	 * @return {Boolean} Returns `true` if there is at least one listener for the specified event.
+	 **/
     willTrigger(type: string) {
-        let o = this;
+        let o: EventDispatcher = this;
         while (o) {
             if (o.hasEventListener(type)) { return true; }
 
@@ -140,12 +355,24 @@ export class iEventDispatcher {
         return false;
     };
 
-
+    /**
+	 * @method toString
+	 * @return {String} a string representation of the instance.
+	 **/
     toString(): string {
         return "[EventDispatcher]";
     };
 
-
+        // private methods:
+    /**
+	 * @method _dispatchEvent
+	 * @param {Object | String | Event} eventObj
+	 * @param {Object} eventPhase
+	 * @protected
+	 **/
+    _dispatchEvent(eventObj: Object, eventPhase: Object): boolean;
+    _dispatchEvent(eventObj: String, eventPhase: Object): boolean;
+    _dispatchEvent(eventObj: Event, eventphase: Object): boolean;
     _dispatchEvent(eventObj, eventPhase): boolean {
         var l, listeners = (eventPhase == 1) ? this._captureListeners : this._listeners;
         if (eventObj && listeners) {
